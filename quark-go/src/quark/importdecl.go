e3039e62abd26a4c70915cb7a972dfa5c87af8b0
Ethan Hunter <fc.spaceman@gmail.com>
2020-01-10 16:23:44 -0700

    add importdecl implementations

--- quark-go/src/quark/importdecl.go
+++ quark-go/src/quark/importdecl.go
@@ -1,1 +1,33 @@
 package quark
+
+type (
+	genericImport struct {
+		PackageName Name
+
+		kwImport ObjectPosition
+	}
+
+	SingleImport struct {
+		genericImport
+	}
+
+	WildcardImport struct {
+		genericImport
+
+		star ObjectPosition
+	}
+)
+
+func (i *genericImport) Start() *ObjectPosition {
+	return &i.kwImport
+}
+
+func (i *genericImport) End() *ObjectPosition {
+	return i.PackageName.End()
+}
+
+func (w *WildcardImport) End() *ObjectPosition {
+	return w.star.Next()
+}
+
+func (i *genericImport) importDeclNode() {}
