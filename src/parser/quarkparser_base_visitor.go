bcdaa1c02a939dfefee1414826856d47b5b7c56a
Ethan Hunter <fc.spaceman@gmail.com>
2020-01-17 12:52:21 -0700

    follow go convention of checking in generated code

---
+++ src/parser/quarkparser_base_visitor.go
@@ -0,0 +1,325 @@
+// Code generated from src/parser/QuarkParser.g4 by ANTLR 4.7.2. DO NOT EDIT.
+
+package parser // QuarkParser
+
+import "github.com/antlr/antlr4/runtime/Go/antlr"
+
+type BaseQuarkParserVisitor struct {
+	*antlr.BaseParseTreeVisitor
+}
+
+func (v *BaseQuarkParserVisitor) VisitQuarkpackage(ctx *QuarkpackageContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitDecl(ctx *DeclContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitSingleImport(ctx *SingleImportContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitWildcardImport(ctx *WildcardImportContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitMultiImport(ctx *MultiImportContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitBlock(ctx *BlockContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitAssignment(ctx *AssignmentContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitAssignStmt(ctx *AssignStmtContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitRegAssignStmt(ctx *RegAssignStmtContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitDeclarationStmt(ctx *DeclarationStmtContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitBranchStmt(ctx *BranchStmtContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitFutureStmt(ctx *FutureStmtContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitReturnStmt(ctx *ReturnStmtContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitFuture(ctx *FutureContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitValueAssignment(ctx *ValueAssignmentContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitVariableDefinition(ctx *VariableDefinitionContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitTupleDestructer(ctx *TupleDestructerContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitArrayIndexAssignment(ctx *ArrayIndexAssignmentContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitArraySliceAssignment(ctx *ArraySliceAssignmentContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitRealname(ctx *RealnameContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitUnqualifiedName(ctx *UnqualifiedNameContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitQualifiedName(ctx *QualifiedNameContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitAtomicClock(ctx *AtomicClockContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitOpenExpr(ctx *OpenExprContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitArrayIndexExpr(ctx *ArrayIndexExprContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitCloseExpr(ctx *CloseExprContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitComplimentExpr(ctx *ComplimentExprContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitArrayLiteralExpr(ctx *ArrayLiteralExprContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitLiteralExpr(ctx *LiteralExprContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitVarExpr(ctx *VarExprContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitBitwiseBinopExpr(ctx *BitwiseBinopExprContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitNotExpr(ctx *NotExprContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitFunctionCall(ctx *FunctionCallContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitShiftExpr(ctx *ShiftExprContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitTernaryExpr(ctx *TernaryExprContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitNewModuleExpr(ctx *NewModuleExprContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitBranchExpr(ctx *BranchExprContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitLambdaExpr(ctx *LambdaExprContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitConstructorExpr(ctx *ConstructorExprContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitTupleExpr(ctx *TupleExprContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitLogicalBinopExpr(ctx *LogicalBinopExprContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitConcatExpr(ctx *ConcatExprContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitMulDivModExpr(ctx *MulDivModExprContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitClockToExpr(ctx *ClockToExprContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitParensExpr(ctx *ParensExprContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitSliceExpr(ctx *SliceExprContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitFieldExpr(ctx *FieldExprContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitAddSubExpr(ctx *AddSubExprContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitCallarglist(ctx *CallarglistContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitNamedCallArg(ctx *NamedCallArgContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitUnamedCallArg(ctx *UnamedCallArgContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitConcat(ctx *ConcatContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitInnerconcat(ctx *InnerconcatContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitParameterizedType(ctx *ParameterizedTypeContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitCompleteType(ctx *CompleteTypeContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitTypeparam(ctx *TypeparamContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitIfBranch(ctx *IfBranchContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitMatchBranch(ctx *MatchBranchContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitAtomicPattern(ctx *AtomicPatternContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitParamerterizedTypePattern(ctx *ParamerterizedTypePatternContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitArrayPattern(ctx *ArrayPatternContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitLiteralPattern(ctx *LiteralPatternContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitStructPattern(ctx *StructPatternContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitParameterlist(ctx *ParameterlistContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitTypeParameter(ctx *TypeParameterContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitValueParameter(ctx *ValueParameterContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitSingleReturn(ctx *SingleReturnContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitNamedReturn(ctx *NamedReturnContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitArgumentdef(ctx *ArgumentdefContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitArgumentlist(ctx *ArgumentlistContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitStructdecl(ctx *StructdeclContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitStructdef(ctx *StructdefContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitFielddecl(ctx *FielddeclContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitInterfacedecl(ctx *InterfacedeclContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitInterfacefield(ctx *InterfacefieldContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitFuncdecl(ctx *FuncdeclContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitModuledecl(ctx *ModuledeclContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitAnnotation(ctx *AnnotationContext) interface{} {
+	return v.VisitChildren(ctx)
+}
+
+func (v *BaseQuarkParserVisitor) VisitLiteral(ctx *LiteralContext) interface{} {
+	return v.VisitChildren(ctx)
+}
---
+++ src/parser/quarkparser_visitor.go
@@ -0,0 +1,247 @@
+// Code generated from src/parser/QuarkParser.g4 by ANTLR 4.7.2. DO NOT EDIT.
+
+package parser // QuarkParser
+
+import "github.com/antlr/antlr4/runtime/Go/antlr"
+
+// A complete Visitor for a parse tree produced by QuarkParser.
+type QuarkParserVisitor interface {
+	antlr.ParseTreeVisitor
+
+	// Visit a parse tree produced by QuarkParser#quarkpackage.
+	VisitQuarkpackage(ctx *QuarkpackageContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#decl.
+	VisitDecl(ctx *DeclContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#SingleImport.
+	VisitSingleImport(ctx *SingleImportContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#WildcardImport.
+	VisitWildcardImport(ctx *WildcardImportContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#MultiImport.
+	VisitMultiImport(ctx *MultiImportContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#block.
+	VisitBlock(ctx *BlockContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#assignment.
+	VisitAssignment(ctx *AssignmentContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#AssignStmt.
+	VisitAssignStmt(ctx *AssignStmtContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#RegAssignStmt.
+	VisitRegAssignStmt(ctx *RegAssignStmtContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#DeclarationStmt.
+	VisitDeclarationStmt(ctx *DeclarationStmtContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#BranchStmt.
+	VisitBranchStmt(ctx *BranchStmtContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#FutureStmt.
+	VisitFutureStmt(ctx *FutureStmtContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#ReturnStmt.
+	VisitReturnStmt(ctx *ReturnStmtContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#future.
+	VisitFuture(ctx *FutureContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#ValueAssignment.
+	VisitValueAssignment(ctx *ValueAssignmentContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#VariableDefinition.
+	VisitVariableDefinition(ctx *VariableDefinitionContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#TupleDestructer.
+	VisitTupleDestructer(ctx *TupleDestructerContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#ArrayIndexAssignment.
+	VisitArrayIndexAssignment(ctx *ArrayIndexAssignmentContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#ArraySliceAssignment.
+	VisitArraySliceAssignment(ctx *ArraySliceAssignmentContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#realname.
+	VisitRealname(ctx *RealnameContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#UnqualifiedName.
+	VisitUnqualifiedName(ctx *UnqualifiedNameContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#QualifiedName.
+	VisitQualifiedName(ctx *QualifiedNameContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#AtomicClock.
+	VisitAtomicClock(ctx *AtomicClockContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#OpenExpr.
+	VisitOpenExpr(ctx *OpenExprContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#ArrayIndexExpr.
+	VisitArrayIndexExpr(ctx *ArrayIndexExprContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#CloseExpr.
+	VisitCloseExpr(ctx *CloseExprContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#ComplimentExpr.
+	VisitComplimentExpr(ctx *ComplimentExprContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#ArrayLiteralExpr.
+	VisitArrayLiteralExpr(ctx *ArrayLiteralExprContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#LiteralExpr.
+	VisitLiteralExpr(ctx *LiteralExprContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#VarExpr.
+	VisitVarExpr(ctx *VarExprContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#BitwiseBinopExpr.
+	VisitBitwiseBinopExpr(ctx *BitwiseBinopExprContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#NotExpr.
+	VisitNotExpr(ctx *NotExprContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#FunctionCall.
+	VisitFunctionCall(ctx *FunctionCallContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#ShiftExpr.
+	VisitShiftExpr(ctx *ShiftExprContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#TernaryExpr.
+	VisitTernaryExpr(ctx *TernaryExprContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#NewModuleExpr.
+	VisitNewModuleExpr(ctx *NewModuleExprContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#BranchExpr.
+	VisitBranchExpr(ctx *BranchExprContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#LambdaExpr.
+	VisitLambdaExpr(ctx *LambdaExprContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#ConstructorExpr.
+	VisitConstructorExpr(ctx *ConstructorExprContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#TupleExpr.
+	VisitTupleExpr(ctx *TupleExprContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#LogicalBinopExpr.
+	VisitLogicalBinopExpr(ctx *LogicalBinopExprContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#ConcatExpr.
+	VisitConcatExpr(ctx *ConcatExprContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#MulDivModExpr.
+	VisitMulDivModExpr(ctx *MulDivModExprContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#ClockToExpr.
+	VisitClockToExpr(ctx *ClockToExprContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#ParensExpr.
+	VisitParensExpr(ctx *ParensExprContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#SliceExpr.
+	VisitSliceExpr(ctx *SliceExprContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#FieldExpr.
+	VisitFieldExpr(ctx *FieldExprContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#AddSubExpr.
+	VisitAddSubExpr(ctx *AddSubExprContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#callarglist.
+	VisitCallarglist(ctx *CallarglistContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#NamedCallArg.
+	VisitNamedCallArg(ctx *NamedCallArgContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#UnamedCallArg.
+	VisitUnamedCallArg(ctx *UnamedCallArgContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#concat.
+	VisitConcat(ctx *ConcatContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#innerconcat.
+	VisitInnerconcat(ctx *InnerconcatContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#ParameterizedType.
+	VisitParameterizedType(ctx *ParameterizedTypeContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#CompleteType.
+	VisitCompleteType(ctx *CompleteTypeContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#typeparam.
+	VisitTypeparam(ctx *TypeparamContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#IfBranch.
+	VisitIfBranch(ctx *IfBranchContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#MatchBranch.
+	VisitMatchBranch(ctx *MatchBranchContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#AtomicPattern.
+	VisitAtomicPattern(ctx *AtomicPatternContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#ParamerterizedTypePattern.
+	VisitParamerterizedTypePattern(ctx *ParamerterizedTypePatternContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#ArrayPattern.
+	VisitArrayPattern(ctx *ArrayPatternContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#LiteralPattern.
+	VisitLiteralPattern(ctx *LiteralPatternContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#StructPattern.
+	VisitStructPattern(ctx *StructPatternContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#parameterlist.
+	VisitParameterlist(ctx *ParameterlistContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#TypeParameter.
+	VisitTypeParameter(ctx *TypeParameterContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#ValueParameter.
+	VisitValueParameter(ctx *ValueParameterContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#SingleReturn.
+	VisitSingleReturn(ctx *SingleReturnContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#NamedReturn.
+	VisitNamedReturn(ctx *NamedReturnContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#argumentdef.
+	VisitArgumentdef(ctx *ArgumentdefContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#argumentlist.
+	VisitArgumentlist(ctx *ArgumentlistContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#structdecl.
+	VisitStructdecl(ctx *StructdeclContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#structdef.
+	VisitStructdef(ctx *StructdefContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#fielddecl.
+	VisitFielddecl(ctx *FielddeclContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#interfacedecl.
+	VisitInterfacedecl(ctx *InterfacedeclContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#interfacefield.
+	VisitInterfacefield(ctx *InterfacefieldContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#funcdecl.
+	VisitFuncdecl(ctx *FuncdeclContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#moduledecl.
+	VisitModuledecl(ctx *ModuledeclContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#annotation.
+	VisitAnnotation(ctx *AnnotationContext) interface{}
+
+	// Visit a parse tree produced by QuarkParser#literal.
+	VisitLiteral(ctx *LiteralContext) interface{}
+}
